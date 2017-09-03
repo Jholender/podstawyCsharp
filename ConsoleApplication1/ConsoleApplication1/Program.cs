dd918cf339525c4d2b1c696d4c9057aa3340c26d
Jarek Holender <jholender@op.pl>
2017-09-03 13:36:35 +0200

    pierwsze

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,10 +10,14 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cześć, ile masz lat?");
-            string wiek;
-                wiek = Console.ReadLine();
-            Console.WriteLine("Masz " + wiek + " lat. Super?");
+            Console.WriteLine("Cześć, jak masz na imię?");
+            string imie;
+                imie = Console.ReadLine();
+            Console.WriteLine("Ile masz lat?");
+                string wiek;
+            wiek = Console.ReadLine();
+
+            Console.WriteLine("Masz na imię " + imie + " i masz" + wiek + " lat");
             Console.ReadLine();
         }
     }
